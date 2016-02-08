785c01f7e5864e9c21018b4fa5b74e20d88852ca
sewert <spencer.ewert@gmail.com>
2016-02-08 13:39:42 -0700

    Main menu view

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,4 +1,4 @@
-exports.render = function(req, res) {
-    var path = require('path');
-    res.sendFile(path.join(__dirname, '../views', 'index.html'));
+exports.getPage = function(req, res) {
+    var path = require("path");
+    res.sendFile(path.join(__dirname, "../views", "index.html"));
 };
--- public/js/game.js
+++ public/js/game.js
@@ -1,14 +1,32 @@
-var game = new Phaser.Game(1024, 768, Phaser.AUTO, "game", { preload: preload, create: create, update: update });
+var gameHeight = 768;
+var gameWidth = 1024;
+var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, "game", { preload: preload, create: create, update: update });
 
 function preload() {
-    game.load.image("background", "../assets/backgrounds/background.jpg");
+    game.load.image("background", "../assets/backgrounds/SpaceBackground-2.jpg");
+    game.load.image("newGameButton", "../assets/buttons/newGame_raised.png");
+    game.load.image("saveGameButton", "../assets/buttons/saveGame_raised.png");
+    game.load.image("loadGameButton", "../assets/buttons/loadGame_raised.png");
+    game.load.image("tutorialButton", "../assets/buttons/tutorial_raised.png");
 }
 
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.add.sprite(0, 0, "background");
+
+    showMenu();
 }
 
+function showMenu() {
+    newGameButton = game.add.sprite(gameWidth / 2, 184, "newGameButton");
+    newGameButton.anchor.setTo(0.5, 0.5);
+    saveGameButton = game.add.sprite(gameWidth / 2, 294, "saveGameButton");
+    saveGameButton.anchor.setTo(0.5, 0.5);
+    loadGameButton = game.add.sprite(gameWidth / 2, 404, "loadGameButton");
+    loadGameButton.anchor.setTo(0.5, 0.5);
+    tutorialButton = game.add.sprite(gameWidth / 2, 514, "tutorialButton");
+    tutorialButton.anchor.setTo(0.5, 0.5);
+}
 function update() {
 
 }
