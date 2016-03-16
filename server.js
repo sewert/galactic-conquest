3d3a3674636894a19cd6df2e57892f2b65b5f686
sewert <spencer.ewert@gmail.com>
2016-03-15 17:58:02 -1000

    Socket.io hello world

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var port = 1337;
 var express = require("./config/express");
 var app = express();
-app.listen(port);
-module.exports = app;
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+server.listen(port);
 console.log("Server running at http://localhost:" + port);
+
+io.on("connection", function (client) {
+  console.log("Client connected...");
+
+  client.on("join", function(data) {
+    console.log(data);
+  });
+
+  client.on("messages", function(data) {
+    console.log(data);
+    client.emit("broad", data);
+    client.broadcast.emit("broad", data);
+  });
+});
