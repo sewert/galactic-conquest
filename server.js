fdd6a41b60b09742796d884e2a95fe9ff464d708
sewert <spencer.ewert@gmail.com>
2016-03-21 19:47:09 -0600

    Better chat. Still has styling issues. Uses jQuery and Socket.io

--- server.js
+++ server.js
@@ -6,16 +6,63 @@ var io = require("socket.io")(server);
 server.listen(port);
 console.log("Server running at http://localhost:" + port);
 
-io.on("connection", function (client) {
+var playerCount = 0;
+var MAX_PLAYERS = 6;
+
+io.on("connection", function (socket) {
   console.log("Client connected...");
 
-  client.on("join", function(data) {
-    console.log(data);
+  var playerAdded = false;
+
+  // handle join requests from client
+  socket.on("addPlayer", function(playerName) {
+    console.log(playerName);
+
+    if (playerAdded) return;
+    if (playerCount >= MAX_PLAYERS) return;
+
+    socket.playerName = playerName;
+    ++playerCount;
+    playerAdded = true;
+    socket.emit("login", {
+      playerCount: playerCount
+    });
+
+    socket.broadcast.emit("playerAdded", {
+      playerName: socket.playerName,
+      playerCount: playerCount
+    });
   });
 
-  client.on("chatMessage", function(data) {
+  // show when client is typing
+  socket.on("typing", function() {
+    socket.broadcast.emit("typing", {
+      playerName: socket.playerName
+    });
+  });
+
+  socket.on("stopTyping", function() {
+    socket.broadcast.emit("stopTyping", {
+      playerName: socket.playerName
+    });
+  });
+
+  socket.on("disconnect", function() {
+    if (playerAdded) {
+      --playerCount;
+      socket.broadcast.emit("playerLeft", {
+        playerName: socket.playerName,
+        playerCount: playerCount
+      });
+    }
+  });
+
+  socket.on("newMessage", function(data) {
     console.log(data);
-    client.emit("chatMessage", data);
-    client.broadcast.emit("chatMessage", data);
+
+    socket.broadcast.emit("newMessage", {
+      playerName: socket.playerName,
+      message: data
+    });
   });
 });
