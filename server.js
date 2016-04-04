60e70cab56e98d83898ec2e635865d083b338bc7
sewert <spencer.ewert@gmail.com>
2016-04-04 16:28:01 -0600

    Code cleanup

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ var mongoClient = require("mongodb").MongoClient;
 var ObjectID = require("mongodb").ObjectID;
 var url = "mongodb://<username>:<password>@ds025379.mlab.com:25379/galactic-conquest";
 mongoClient.connect(url, function (err, db) {
-  if(err) throw err;
-  mongoClient.savedGamesCollection = db.collection("savedGames");
-  findSavedGame(mongoClient.savedGamesCollection, "56f5a6e2e4b080b1e46143b3", function() {
-    db.close();
-  });
+    if (err) throw err;
+    mongoClient.savedGamesCollection = db.collection("savedGames");
+    findSavedGame(mongoClient.savedGamesCollection, "56f5a6e2e4b080b1e46143b3", function () {
+        db.close();
+    });
 });
 
 function findSavedGame(collection, data, callback) {
-  collection.find({"_id": new ObjectID(data)}).toArray(function(err, results) {
-    players = results[0].players;
-    currentPlayersTurn = results[0].currentTurn;
-    tiles = results[0].tiles;
-  });
+    collection.find({"_id": new ObjectID(data)}).toArray(function (err, results) {
+        players = results[0].players;
+        currentPlayersTurn = results[0].currentTurn;
+        tiles = results[0].tiles;
+    });
 }
 
 // Start up server
@@ -33,137 +33,137 @@ var players;
 var currentPlayersTurn;
 var tiles;
 io.on("connection", function (socket) {
-  var playerAdded = false;
+    var playerAdded = false;
 
-  // handle join requests from client
-  socket.on("addPlayer", function(playerName) {
-    if (playerAdded) return;
-    if (playerCount >= MAX_PLAYERS) return; // TODO: emit error message
+    // handle join requests from client
+    socket.on("addPlayer", function (playerName) {
+        if (playerAdded) return;
+        if (playerCount >= MAX_PLAYERS) return; // TODO: emit error message
 
-    socket.playerName = playerName;
-    ++playerCount;
-    playerAdded = true;
-    socket.emit("login", {
-      playerCount: playerCount
+        socket.playerName = playerName;
+        ++playerCount;
+        playerAdded = true;
+        socket.emit("login", {
+            playerCount: playerCount
+        });
+
+        socket.broadcast.emit("playerAdded", {
+            playerName: socket.playerName,
+            playerCount: playerCount
+        });
+        console.log(playerName + " joined");
     });
 
-    socket.broadcast.emit("playerAdded", {
-      playerName: socket.playerName,
-      playerCount: playerCount
+    // Chat feature
+    socket.on("typing", function () {
+        socket.broadcast.emit("typing", {
+            playerName: socket.playerName
+        });
     });
-    console.log(playerName + " joined");
-  });
 
-  // Chat feature
-  socket.on("typing", function() {
-    socket.broadcast.emit("typing", {
-      playerName: socket.playerName
+    socket.on("stopTyping", function () {
+        socket.broadcast.emit("stopTyping", {
+            playerName: socket.playerName
+        });
     });
-  });
 
-  socket.on("stopTyping", function() {
-    socket.broadcast.emit("stopTyping", {
-      playerName: socket.playerName
+    socket.on("disconnect", function () {
+        if (playerAdded) {
+            --playerCount;
+            socket.broadcast.emit("playerLeft", {
+                playerName: socket.playerName,
+                playerCount: playerCount
+            });
+            console.log(socket.playerName + " disconnected");
+        }
     });
-  });
 
-  socket.on("disconnect", function() {
-    if (playerAdded) {
-      --playerCount;
-      socket.broadcast.emit("playerLeft", {
-        playerName: socket.playerName,
-        playerCount: playerCount
-      });
-      console.log(socket.playerName + " disconnected");
-    }
-  });
-
-  socket.on("newMessage", function(data) {
-    socket.broadcast.emit("newMessage", {
-      playerName: socket.playerName,
-      message: data
+    socket.on("newMessage", function (data) {
+        socket.broadcast.emit("newMessage", {
+            playerName: socket.playerName,
+            message: data
+        });
     });
-  });
 
-  // Menu
-  socket.on("loadGame", function(data) {
-    //TODO: write me!
-  });
-
-  socket.on("newGame", function() {
-    //TODO: write me!
-  });
-
-  socket.on("saveGame", function() {
-    //TODO: write me!
-  });
-
-  // Player Game Actions
-  socket.on("activateSystem", function(data) {
-    //TODO: write me for reals
-  });
-
-  socket.on("buildShips", function(data) {
-    //TODO: write me!
-  });
-
-  socket.on("endTurn", function(data) {
-    if (currentPlayersTurn === data) {
-      checkVictoryConditions();
-      currentPlayersTurn = findNextPlayersTurn(data);
-      socket.broadcast.emit("updateTurn", currentPlayersTurn);
-      socket.emit("updateTurn", currentPlayersTurn);
-    }
-  });
-
-  socket.on("sendShips", function(data) {
-    //TODO: write me!
-  });
-
-  socket.on("updatePlanet", function(data) {
-    var planetData = getPlanetInfo(data);
-    socket.emit("updatePlanet", {
-      ownerName: planetData.ownerName,
-      planetName: planetData.planetName,
-      fighters: planetData.fighters,
-      destroyers: planetData.destroyers,
-      dreadnoughts: planetData.dreadnoughts
+    // Menu
+    socket.on("loadGame", function (data) {
+        //TODO: write me!
     });
-  });
 
-  socket.on("updateTurn", function() {
-    socket.emit("updateTurn", currentPlayersTurn);
-  });
+    socket.on("newGame", function () {
+        //TODO: write me!
+    });
+
+    socket.on("saveGame", function () {
+        //TODO: write me!
+    });
+
+    // Player Game Actions
+    socket.on("activateSystem", function (data) {
+        //TODO: write me for reals
+    });
+
+    socket.on("buildShips", function (data) {
+        //TODO: write me!
+    });
+
+    socket.on("endTurn", function (data) {
+        if (currentPlayersTurn === data) {
+            checkVictoryConditions();
+            currentPlayersTurn = findNextPlayersTurn(data);
+            socket.broadcast.emit("updateTurn", currentPlayersTurn);
+            socket.emit("updateTurn", currentPlayersTurn);
+        }
+    });
+
+    socket.on("sendShips", function (data) {
+        //TODO: write me!
+    });
+
+    socket.on("updatePlanet", function (data) {
+        var planetData = getPlanetInfo(data);
+        socket.emit("updatePlanet", {
+            ownerName: planetData.ownerName,
+            planetName: planetData.planetName,
+            fighters: planetData.fighters,
+            destroyers: planetData.destroyers,
+            dreadnoughts: planetData.dreadnoughts
+        });
+    });
+
+    socket.on("updateTurn", function () {
+        socket.emit("updateTurn", currentPlayersTurn);
+    });
 });
 
 function checkVictoryConditions() {
-  // TODO: write me!
+    // TODO: write me!
 }
 
 function findNextPlayersTurn(data) {
-  for (var i = 0; i < players.length; i++) {
-    if (players[i].name === data) {
-      if (i + 1 < players.length) {
-        return players[i+1].name;
-      }
-      else {
-        return players[0].name;
-      }
+    for (var i = 0; i < players.length; i++) {
+        if (players[i].name === data) {
+            if (i + 1 < players.length) {
+                return players[i + 1].name;
+            }
+            else {
+                return players[0].name;
+            }
+        }
     }
-  }
 }
 
 function getPlanetInfo(data) {
-  for (var i = 0; i < tiles.length; i++) {
-    if (tiles[i].name === data) {
-      data = {
-        ownerName: tiles[i].owner,
-        planetName: tiles[i].name,
-        fighters: tiles[i].fighters,
-        destroyers: tiles[i].destroyers,
-        dreadnoughts: tiles[i].dreadnoughts
-      }
-      return data;
+    for (var i = 0; i < tiles.length; i++) {
+        if (tiles[i].name === data) {
+            data = {
+                ownerName: tiles[i].owner,
+                planetName: tiles[i].name,
+                fighters: tiles[i].fighters,
+                destroyers: tiles[i].destroyers,
+                dreadnoughts: tiles[i].dreadnoughts
+            };
+            return data;
+        }
     }
-  }
 }
